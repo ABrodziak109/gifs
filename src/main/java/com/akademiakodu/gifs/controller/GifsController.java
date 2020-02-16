694dba69ac6173d6cc00a673e188f5f349cb9460
Angelika Brodziak <brodziak.angelika@gmail.com>
2020-02-16 12:09:07 +0100

    Adding new method

--- src/main/java/com/akademiakodu/gifs/controller/GifsController.java
+++ src/main/java/com/akademiakodu/gifs/controller/GifsController.java
@@ -1,17 +1,36 @@
 package com.akademiakodu.gifs.controller;
 
+import com.akademiakodu.gifs.model.Gif;
+import com.akademiakodu.gifs.repository.GifRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 
 public class GifsController {
 
 
+    final GifRepository gifRepository;
+
+    public GifsController(GifRepository gifRepository){
+        this.gifRepository = gifRepository;
+    }
+
     @GetMapping("/")
     @ResponseBody
     public String hello() {
         return "działa";
     }
+
+    @GetMapping("/showgifinbrowser")
+    @ResponseBody
+    public String showGifNames(){
+        return gifRepository.getGifNames();
+    }
+
+
 }
---
+++ src/main/java/com/akademiakodu/gifs/model/Gif.java
@@ -0,0 +1,49 @@
+package com.akademiakodu.gifs.model;
+
+public class Gif {
+
+    private String name;
+    private String userName;
+    private Boolean favorite;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Boolean getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(Boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    private int categoryId;
+
+    public Gif(String name, String userName, Boolean favorite, int categoryId) {
+        this.name = name;
+        this.userName = userName;
+        this.favorite = favorite;
+        this.categoryId = categoryId;
+    }
+}
